ce123dba9abc14cac50ce5e1959535ff6fad14bd
Kazi Tajnur Islam <kazi.tajnur@vivacomsolutions.com>
2024-01-02 15:11:27 +0600

    Add Metropolis custom font and update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
 import { Providers } from '../components/providers/redux-providers';
 import './globals.css';
-
-const roboto = Roboto({
-  display: 'swap',
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
-});
 
 export const metadata: Metadata = {
   title: 'D2D Marketing',
@@ -20,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <Providers>
       <html lang='en'>
-        <body className={roboto.className}>{children}</body>
+        <body className=''>{children}</body>
       </html>
     </Providers>
   );
