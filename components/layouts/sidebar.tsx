150af599bc8e79716148b7eed54e6b32493f8e9a
rkshawon <rkshawon36@gmail.com>
2024-01-03 15:16:51 +0600

    fixed svg issue in sidebar

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -2,27 +2,53 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
-import HomeIcon from '../../public/navbar/home';
-import BookIcon from '../../public/navbar/book';
-import SettingIcon from '../../public/navbar/setting';
 
 const sidebarItem = [
   {
     id: 1,
     iconName: 'Home',
-    icon: <HomeIcon />,
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='currentColor'
+        className='w-6 h-6'>
+        <path d='M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z' />
+        <path d='m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z' />
+      </svg>
+    ),
     position: -15,
   },
   {
     id: 2,
     iconName: 'Book',
-    icon: <BookIcon />,
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='currentColor'
+        className='w-6 h-6'>
+        <path d='M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z' />
+      </svg>
+    ),
     position: 50,
   },
   {
     id: 3,
     iconName: 'Setting',
-    icon: <SettingIcon />,
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='currentColor'
+        className='w-6 h-6'>
+        <path
+          fillRule='evenodd'
+          d='M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z'
+          clipRule='evenodd'
+        />
+      </svg>
+    ),
     position: 110,
   },
 ];
@@ -50,7 +76,13 @@ const Sidebar = () => {
           );
         })}
         <div
-          className={`absolute left-0 transition-all duration-500 top-[${selected}px]  h-[50px] w-1 bg-[#5630FF] rounded-tr rounded-br `}></div>
+          className={`absolute left-0 transition-all duration-500 ${
+            selected === 110
+              ? 'top-[110px]'
+              : selected === 50
+              ? 'top-[50px]'
+              : 'top-[-15px]'
+          }  h-[50px] w-1 bg-[#5630FF] rounded-tr rounded-br `}></div>
       </div>
       <div className='absolute w-full flex justify-center bottom-5'>
         <Image src='/navbar/Avatar.png' alt='' height={46} width={46} />
