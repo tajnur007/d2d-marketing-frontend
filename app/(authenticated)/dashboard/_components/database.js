3b2e2d2a959ebdd686fa6607249299e79c7e61fd
Hridoy <19202103103@cse.bubt.edu.bd>
2024-01-03 18:22:15 +0600

    leaderBoard

---
+++ app/(authenticated)/dashboard/_components/database.js
@@ -0,0 +1,106 @@
+export const Leaderboard = [
+    { 
+        id: 1,
+        initials: 'AA', 
+        name: 'Ashiqul Amin', 
+        role: 'of leads', 
+        username: 'ashiqulamin', 
+        totalLeads:34 
+    },
+    { 
+        id: 2,
+        initials: 'HM', 
+        name: 'Hussain Al Muhee', 
+        role: 'of leads', 
+        username: 'ashiqulamin', 
+        totalLeads:31 
+    },
+    { 
+        id: 3,
+        initials: 'BS', 
+        name: 'Biplab Chandra Sarker', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:35 
+    },
+    { 
+        id: 4,
+        initials: 'AT', 
+        name: 'Ahnaf Tahmid', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:30 
+    },
+    { 
+        id: 5,
+        initials: 'HN', 
+        name: 'Humaira Nomrota', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:36 
+    },
+    { 
+        id: 6,
+        initials: 'MH', 
+        name: 'Mashiur Ahmed Hridoy', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:40 
+    },
+    { 
+        id: 7,
+        initials: 'NN', 
+        name: 'Neyem Nawaf', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:40 
+    },
+    { 
+        id: 8,
+        initials: 'AT', 
+        name: 'Ahnaf Tahmid', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:30 
+    },
+    { 
+        id: 9,
+        initials: 'BS', 
+        name: 'Biplab Chandra Sarker', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:35 
+    },
+    { 
+        id: 10,
+        initials: 'AA', 
+        name: 'Ashiqul Amin', 
+        role: 'of leads', 
+        username: 'ashiqulamin', 
+        totalLeads:34 
+    },
+    { 
+        id: 11,
+        initials: 'NN', 
+        name: 'Neyem Nawaf', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:40 
+    },
+    { 
+        id: 12,
+        initials: 'HM', 
+        name: 'Hussain Al Muhee', 
+        role: 'of leads', 
+        username: 'ashiqulamin', 
+        totalLeads:31 
+    },
+    { 
+        id: 7,
+        initials: 'NN', 
+        name: 'Neyem Nawaf', 
+        role: 'of leads', 
+        username: 'ashiqulamin',
+        totalLeads:40 
+    },
+]
--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Board from './_components/board';
+
 
 const DashboardPage = () => {
-  return <div>this is dashboard page</div>;
+  return <div>this is dashboard page
+    <Board/>
+  </div>;
 };
-
 export default DashboardPage;
