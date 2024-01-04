3ef2d468fd7e34931d86e2e30fa94eb458cbad72
Neaz Mahmood <neazn694@gmail.com>
2024-01-04 16:00:26 +0600

    change made on the ./components/copyright.tsx file, removed the fragment and changed the variable name from d to year

--- components/copyright.tsx
+++ components/copyright.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
 const Copyright = () => {
-  const d: number = new Date().getFullYear();
-  return (
-    <>
-      <p className='text-xs font-light'>&copy; {d} ALL RIGHTS RESERVED</p>;
-    </>
-  );
+  const year: number = new Date().getFullYear();
+  return <p className='text-xs font-light'>&copy; {year} ALL RIGHTS RESERVED</p>;
 };
 
 export default Copyright;
