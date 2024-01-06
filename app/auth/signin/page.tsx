7e43c90967958623de619697fb20f16e95283d0f
0emon0 <jahidhxy@gmail.com>
2024-01-06 20:45:57 +0600

    conflict try

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,7 +3,6 @@ import SigninForm from "@/components/signin-form";
 const SignInPage = () => {
   return (
     <div className=''>
-      This is <strong>Signin</strong> page
       <SigninForm />
     </div>
     
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,10 +1,8 @@
 import SignupForm from "@/components/signup-form";
 
-
 const SignUpPage = () => {
   return (
     <div className=''>
-      This is <strong>Signup</strong> page
       <SignupForm />
     </div>
   );
