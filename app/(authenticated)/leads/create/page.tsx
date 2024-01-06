14f9c243eaddc22a872690eac79535be5bb46802
rkshaown <rkshawon36@gmail.com>
2024-01-06 16:59:59 +0600

    Implemented back functionality

--- app/(authenticated)/leads/create/page.tsx
+++ app/(authenticated)/leads/create/page.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import { BackArrowIcon } from '@/assets/icons';
 import CreateLeadForm from '@/components/createlead/create-lead-form';
+import { useRouter } from 'next/navigation';
 
 const LeadsCreatePage = () => {
+  const router = useRouter();
   return (
     <div className='bg-white rounded-[10px] p-6'>
       <div className='flex items-center gap-5'>
-        <BackArrowIcon />
+        <p onClick={() => router.back()} className=' cursor-pointer'>
+          <BackArrowIcon />
+        </p>
         <p className='text-[#25254C] text-xl font-medium'>Create</p>
       </div>
       <CreateLeadForm />
