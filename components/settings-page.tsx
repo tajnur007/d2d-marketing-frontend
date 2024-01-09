7bd7b7efb06388a0fc7222269fd44f4d5c57fbde
Neaz Mahmood <neazn694@gmail.com>
2024-01-09 16:43:44 +0600

    primary design done, avater change function and password change function pending

--- components/settings-page.tsx
+++ components/settings-page.tsx
@@ -1,39 +1,85 @@
 'use client';
 
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import Image from 'next/image';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { FormItems } from '@/models/global-types';
+import { FORMITEMS } from '@/utils/constants/common-constants';
+import profileImage from '@/assets/images/profile.png';
 
 const SettingsPage = () => {
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [selected, setSelected] = useState('Pending');
+  const [formData, setFormData] = useState<FormItems>(FORMITEMS);
+  const [formErrors, setFormErrors] = useState<FormItems>(FORMITEMS);
 
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
 
-  const handlePasswordVisibilityToggle = () => {
-    setShowPassword(!showPassword);
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+
+    setFormErrors((prev) => {
+      return { ...prev, [name]: '' };
+    });
   };
 
-  const handleConfirmPasswordVisibilityToggle = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
+  useEffect(() => {
+    setFormData((prev) => {
+      return { ...prev, Status: selected };
+    });
+  }, [selected, formErrors]);
 
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value);
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setEmail(e.target.value);
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setPassword(e.target.value);
-  const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setConfirmPassword(e.target.value);
+  const submitData = () => {
+    const newFormErrors: any = {};
 
-  const handleSubmit = () => {
-    console.log('Form submitted:', { name, email, password, confirmPassword });
+    for (let field in formData) {
+      if (formData[field as keyof typeof formData] === '') {
+        newFormErrors[field] = `(${field} is required)`;
+      }
+    }
+    setFormErrors(newFormErrors);
   };
 
   return (
-    <section className='flex justify-center'>
-      <h1>Welcome</h1>
+    <section className='flex justify-center w-full'>
+      <div className='flex flex-col items-center w-full'>
+        <p className='text-4xl font-bold m-8'>Update Profile</p>
+        <form onSubmit={submitData}>
+          <div className='w-full flex justify-center bottom-5'>
+            <Image src={profileImage} alt='' height={46} width={46} />
+          </div>
+          <Input
+            label='Name'
+            placeholder='Name'
+            type='text'
+            id='name'
+            name='Name'
+            errorMessage={formErrors.Name}
+            htmlFor='name'
+            onChange={handleInputChange}
+            className={` ${formErrors.Name && 'border-red-500 shadow'}`}
+          />
+          <Input
+            label='Email'
+            placeholder='Email'
+            type='text'
+            id='email'
+            name='email'
+            errorMessage={formErrors.Name}
+            htmlFor='email'
+            onChange={handleInputChange}
+            className={` ${formErrors.Name && 'border-red-500 shadow'}`}
+          />
+          <Button
+            type='submit'
+            className='text-white text-lg rounded-lg p-3 bg-[#4318FF] '>
+            Save changes
+          </Button>
+        </form>
+      </div>
     </section>
   );
 };
