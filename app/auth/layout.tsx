6e8e71d653e7977b4ced0144add5cf881b7cee92
Hridoy <19202103103@cse.bubt.edu.bd>
2024-01-09 23:05:42 +0600

    Dashboard redirect

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,4 +1,13 @@
+'use client';
+import { PAGE_ROUTES } from '@/utils/constants/common-constants';
+import { useRouter } from 'next/navigation';
+
 const AtuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  let isAuthenticated = false;
+  if (isAuthenticated) {
+    router.push(PAGE_ROUTES.Dashboard);
+  }
   return <div className=''>{children}</div>;
 };
 
