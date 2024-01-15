72379d249b789f21c9fea70960d8a7ba397e8ed0
Saidul-M-Khan <19-41766-3@student.aiub.edu>
2024-01-15 17:13:59 +0600

    All UI Issue Fixed and Added Login Link in Password Changed Page

--- components/auth/new-password.tsx
+++ components/auth/new-password.tsx
@@ -24,42 +24,50 @@ const NewPassword = ({ handleNewPassword }: any) => {
     <section>
       <div className='max-w-[600px] mx-auto px-6 py-12'>
         <div>
-          <div className='mt-10 my-6 text-[16px] flex flex-row text-gray-500 tracking-[0] leading-[16px] whitespace-nowrap'>
-            <Link href={PAGE_ROUTES.Signin} className='mr-2'>
-              <ArrowLeftCircleIcon />
-            </Link>
-            Back to Login
+          <div className='mt-10 my-6 text-[16px] flex items-center text-gray-500 tracking-[0] leading-[16px] whitespace-nowrap'>
+            <div className='mr-2'>
+              <Link href={PAGE_ROUTES.Signin}>
+                <ArrowLeftCircleIcon />
+              </Link>
+            </div>
+            <div>Back to Login</div>
           </div>
           <div className='mt-10 text-[36px] font-bold leading-[36px] tracking-[0.36px] text-[#0B1420] whitespace-nowrap'>
             New password 👋
           </div>
-          <div className='my-5 font-extralight text-[#313957] text-[16px] tracking-[0.16px] leading-[25.6px] whitespace-nowrap'>Type your new password</div>
+          <div className='my-[19px] font-extralight text-[#313957] text-[16px] tracking-[0.16px] leading-[25.6px] whitespace-nowrap'>
+            Type your new password
+          </div>
         </div>
 
         <form onSubmit={onFormSubmit}>
           <div>
-            <label className='font-medium text-[#0B1420] text-[16px] tracking-[0.16px] leading-[16px] whitespace-nowrap'>New password</label>
-            <div className='mt-4 mb-4'>
+            <label className='font-semibold text-[#0B1420] text-[16px] tracking-[0.16px] leading-[16px] whitespace-nowrap'>
+              New password
+            </label>
+            <div className='mt-[8px] mb-[20px]'>
               <input
                 id='newPassword'
                 type='password'
                 name='newPassword'
                 value={newPassword}
-                className='block w-full rounded-[10px] border-0 py-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 pl-2'
+                className='block w-[388px] h-[48px] rounded-[10px] border-0 py-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 pl-2'
                 placeholder='New password'
                 autoComplete='off'
                 required
                 onChange={(event) => setNewPassword(event.target.value)}
               />
             </div>
-            <label className='font-medium text-[#0B1420] text-[16px] tracking-[0.16px] leading-[16px] whitespace-nowrap'>Re-type password</label>
-            <div className='mt-4 mb-4'>
+            <label className='font-semibold text-[#0B1420] text-[16px] tracking-[0.16px] leading-[16px] whitespace-nowrap'>
+              Re-type password
+            </label>
+            <div className='mt-[8px] mb-[20px]'>
               <input
                 id='retypePassword'
                 type='password'
                 name='retypePassword'
                 value={retypePassword}
-                className='block w-full rounded-[10px] border-0 py-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 pl-2'
+                className='block w-[388px] h-[48px] rounded-[10px] border-0 py-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 pl-2'
                 placeholder='Re-type password'
                 autoComplete='off'
                 required
@@ -69,7 +77,8 @@ const NewPassword = ({ handleNewPassword }: any) => {
           </div>
           <ForgetPasswordCommon
             buttonInfo={{
-              className: 'w-full bg-[#4318FF] text-[16px] tracking-[0] leading-[16px] rounded-[12px] text-white p-4 font-normal my-4',
+              className:
+                'w-full bg-[#4318FF] text-[16px] tracking-[0] leading-[16px] rounded-[12px] text-white p-4 font-normal ',
               text: 'Submit',
               type: 'submit',
             }}
--- components/auth/password-changed.tsx
+++ components/auth/password-changed.tsx
@@ -5,31 +5,38 @@ import Link from 'next/link';
 import passwordChangedImage from '@/assets/images/password-changed.png';
 import Copyright from './common/copyright';
 import ForgetPasswordCommon from './common/forget-password-common';
+import { PAGE_ROUTES } from '@/utils/constants/common-constants';
 
 const PasswordChanged = () => {
   return (
     <section>
-      <div className='max-w-[600px] mx-auto px-6 py-12 '>
+      <div className='max-w-[600px] w-[388px] mx-auto py-12 items-center justify-center'>
         <div>
-          <div className='mt-10 my-6 text-[16px] flex flex-row text-gray-500'></div>
-          <div className='mt-10 text-[36px] font-bold leading-[36px] tracking-[0.36px] text-[#0B1420] whitespace-nowrap'>
+          <div className='mt-10 w-[388px] my-6 text-[16px] flex flex-row text-gray-500'></div>
+          <div className='mt-10 w-[388px] text-[36px] font-bold leading-[36px] tracking-[0.36px] text-[#0B1420] whitespace-nowrap'>
             Password changed!
           </div>
-          <div className='w-[600px] my-5 text-[16px] text-[#313957] font-extralight tracking-[0.16px] leading-[25.6px] whitespace-nowrap'>
+          <div className='w-[388px] my-5 text-[16px] text-[#313957] font-extralight tracking-[0.16px] leading-[25.6px] whitespace-nowrap'>
             Your password has been updated successfully.
             <br />
           </div>
         </div>
-        <div className='my-4  flex items-center justify-center '>
+        <div className='my-4 flex items-center justify-center'>
           <Image src={passwordChangedImage} alt='page-logo' />
         </div>
-        <ForgetPasswordCommon
-          buttonInfo={{
-            className: 'w-full bg-[#4318FF] rounded-[12px] text-[16px] tracking-[0] leading-[16px] text-white p-4 font-extralight my-4',
-            text: 'Login now',
-            type: 'text',
-          }}
-        />
+        <div className='flex items-center justify-center'>
+          <Link href={PAGE_ROUTES.Signin}>
+            <ForgetPasswordCommon
+              buttonInfo={{
+                className:
+                  'w-[388px] bg-[#4318FF] rounded-[12px] text-[16px] tracking-[0] leading-[16px] text-white p-4 font-extralight mt-4 mb-[55px]',
+                text: 'Login now',
+                type: 'text',
+              }}
+            />
+          </Link>
+        </div>
+
         <Copyright />
       </div>
     </section>
