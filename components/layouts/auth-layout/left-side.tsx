f655005265f61742c74b033de30c76254bba7147
Neaz Mahmood <neazn694@gmail.com>
2024-01-15 14:55:09 +0600

    fixed auth layout

--- components/layouts/auth-layout/left-side.tsx
+++ components/layouts/auth-layout/left-side.tsx
@@ -6,22 +6,24 @@ import { AuthCommonProps } from '@/models/global-types';
 
 const LeftSide = ({ pageImage, text }: AuthCommonProps) => {
   return (
-    <div className='w-2/5 h-screen relative flex overflow-hidden bg-primary-bg  text-white p-0 m-0'>
-      <div className='absolute w-full overflow-hidden top-0 left-0  flex z-1 justify-between items-start'>
+    <div className='w-2/5 h-screen overflow-hidden relative bg-primary-bg  text-white p-20 lg:p-0 m-0 container block'>
+      <div className='absolute w-full top-0 left-0  flex z-1 justify-between items-start'>
         <Image style={{ width: '100%' }} src={layoutTop} alt='top' />
       </div>
-      <div className='flex flex-col gap-36 relative '>
-        <div className='pl-[58px] pt-[63px]'>
+      <div className='h-full flex flex-col justify-between items-start relative'>
+        <div className='lg:p-10 lg:pt-20'>
           <Image src={d2dIcon} alt='icon' />
         </div>
-        <div className='pl-14'>
+        <div className='w-full lg:p-10'>
           <div className='text-base'>
-            <h5>Door 2 door marketing 🙂</h5>
+            <h5 className='2xl:text-3xl'>Door 2 door marketing 🙂</h5>
           </div>
-          <div className='pt-6 w-[501px] h-[33px] text-[14px] text-[#FFFFFF80] tracking-[0] leading-[20px]'>{text}</div>
+          <div className='pt-6 lg:pt-2 2xl:text-xl text-[14px] text-[#FFFFFF80] tracking-[0] leading-[20px]'>
+            {text}
+          </div>
         </div>
-        <div className='pl-[37px] pd-[86px] pr-[38px]'>
-          <Image src={pageImage} alt='marketing' />
+        <div className='container flex w-full p-20 xl:p-20'>
+          <Image className='object-contain w-full' src={pageImage} alt='marketing' />
         </div>
       </div>
     </div>
