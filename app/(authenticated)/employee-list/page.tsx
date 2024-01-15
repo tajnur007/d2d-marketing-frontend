45d669ed502711b3cdb6c1494880c203fed3a1fd
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-01-16 01:19:54 +0600

    employee list page added

---
+++ app/(authenticated)/employee-list/page.tsx
@@ -0,0 +1,10 @@
+
+const EmployeeListPage = () => {
+  return (
+    <div className='flex flex-col lg:flex-row justify-between gap-6'>
+      <h1>This is Employee List Page</h1>
+    </div>
+  );
+};
+
+export default EmployeeListPage;
