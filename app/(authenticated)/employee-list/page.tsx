6d393394a689e0a685fba324445ad0887e0d2a6e
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-01-19 01:56:09 +0600

    figma design build

--- app/(authenticated)/employee-list/page.tsx
+++ app/(authenticated)/employee-list/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { EMPLOYEE_LIST_DATA } from '@/utils/constants/employee-list-constant';
-import LeadRow from '@/components/LeadRow/LeadRow';
 import Image from 'next/image';
 import plusImage from '@/assets/images/leadslist-icons/add-circle.png';
 import profileImage from '@/assets/images/profilePic.png';
@@ -17,32 +16,75 @@ const EmployeeListPage = () => {
   return (
     <div className='border border-gray-100 bg-white rounded-xl h-[88vh] w-full'>
       <div className='py-4 md:py-6 pl-8 h-[96px]'>
-        <div className='flex justify-between items-center content-center'>
+        <div className='flex justify-between items-center'>
           <div className='flex items-center'>
             <div>
-              <p className="[font-family:'Metropolis-Bold',Helvetica] font-semibold text-[16px] tracking-[-0.32px] leading-[normal] whitespace-nowrap text-capitalize text-[#2B3674]">
+              <p className='font-semibold text-[16px] tracking-[-0.32px] leading-[normal] whitespace-nowrap text-capitalize text-[#2B3674]'>
                 Employee List
               </p>
             </div>
           </div>
-          <div onClick={handleCreateLeadButtonClick}>
-            <button
-              type='button'
-              className='text-white bg-[#5630ff] hover:shadow-blue-500/15 hover:dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-[14px] text-sm p-3 text-center mx-5 transition duration-500 ease-in-out transform hover:-translate-y-1.5 hover:scale-200'>
-              <div className='flex justify-between items-center'>
-                <div className='mr-2'>
-                  <Image src={plusImage} alt='' />
+          <div className='flex flex-row'>
+            <form>
+              <div className='relative'>
+                <div className='absolute inset-y-0 start-0 flex items-center ps-3'>
+                  <svg
+                    className='w-4 h-4 text-gray-500'
+                    aria-hidden='true'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 20 20'>
+                    <path
+                      stroke='currentColor'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                      stroke-width='2'
+                      d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+                    />
+                  </svg>
                 </div>
-                <div className='font-medium text-[14px] leading-[normal] tracking-[0] whitespace-nowrap'>
-                  New Employee
-                </div>
+                <input
+                  type='search'
+                  id='default-search'
+                  className='w-[800px] mx-1 p-3 ps-10 text-md border border-gray-300 rounded-lg'
+                  required
+                />
               </div>
-            </button>
+            </form>
+            <div onClick={handleCreateLeadButtonClick}>
+              <button
+                type='button'
+                className='text-white bg-[#5630ff] hover:shadow-blue-500/15 hover:dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-[14px] text-sm p-3 text-center mx-5 transition duration-500 ease-in-out transform hover:-translate-y-1.5 hover:scale-200'>
+                <div className='flex justify-between items-center'>
+                  <div className='mr-2'>
+                    <Image src={plusImage} alt='' />
+                  </div>
+                  <div className='font-medium text-[14px] leading-[normal] tracking-[0] whitespace-nowrap'>
+                    New Employee
+                  </div>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
 
       <div className='overflow-y-auto overflow-x-hidden tiny-scrollbar h-[71vh]'>
+        <div className='flex justify-between items-center content-center'>
+          <div className='flex items-center'>
+            <div className='ml-12'>
+              <p className=' font-semibold text-[16px] tracking-[-0.32px] leading-[normal] whitespace-nowrap text-capitalize text-[#2B3674]'>
+                A
+              </p>
+            </div>
+
+            <div className='flex items-center justify-center h-6 bg-[#E5DFFF] rounded-[17px] ms-2 p-2'>
+              <p className='leading-[normal] text-black font-semibold text-[16px] tracking-[-0.32px] whitespace-nowrap text-capitalize'>
+                {`7`}
+              </p>
+            </div>
+          </div>
+        </div>
         <div className='w-full px-8 whitespace-nowrap font-medium text-[14px] leading-[normal]'>
           {EMPLOYEE_LIST_DATA.map((item, index) => (
             <EmployeelistRow key={index} item={item} />
@@ -51,7 +93,6 @@ const EmployeeListPage = () => {
       </div>
     </div>
   );
-}
-
+};
 
 export default EmployeeListPage;
--- components/row/employee-list-row.tsx
+++ components/row/employee-list-row.tsx
@@ -1,69 +1,71 @@
 import Image from 'next/image';
-import phoneImage from '@/assets/images/leadslist-icons/call.png';
 import moreImage from '@/assets/images/leadslist-icons/more_vert.png';
-import clockImage from '@/assets/images/leadslist-icons/clock.png';
+import profileImage from '@/assets/images/profilePic.png';
 // import { getStatusColor } from '@/utils/constants/common-constants';
 import { EMPLOYEE_LIST_DATA_TYPE, EmployeestatusColor } from '@/models/global-types';
 
 const getStatusColor: EmployeestatusColor = {
-  Active: 'bg-green-200',
+  Active: "bg-[#D2FBE7]",
   Inactive: 'bg-red-200',
 };
 
 function EmployeelistRow({ item }: { item: EMPLOYEE_LIST_DATA_TYPE }) {
   return (
     <div>
+
       <div
         key={item.id}
-        className='h-20 flex gap-5 justify-between items-center text-sm leading-none text-gray-800 border-b border-white'>
-        <div className='w-[30%]'>
-          <p className='leading-trim font-semibold text-[18px] tracking-tight'>
+        className='h-20 flex gap-5 justify-between items-center text-sm leading-none text-gray-800 border-b mb-1'>
+        <div className='w-5%'>
+          <div className='flex items-center w-15 mt-[8px] '>
+            <Image
+              src={profileImage}
+              alt='Profile Picture'
+              className='rounded-full w-12 h-12 flex-shrink-0'
+            />
+          </div>
+        </div>
+
+        <div className='w-[25%]'>
+          <p className='leading-trim font-bold text-[16px] tracking-tight mt-2'>
             {item.employeeName}
           </p>
-          <div className='flex items-center mt-[14px]'>
-            <div className='mr-1'>
-              <Image src={clockImage} alt='' />
-            </div>
+          <div className='flex items-center mt-[4px]'>
             <div className='text-[#9d9d9d] text-[12px] leading-[14.5px] tracking-[0] whitespace-nowrap text-capitalize inline-block'>
               {item.employeeDesignation}
             </div>
           </div>
         </div>
+
         <div className='w-[25%]'>
-          <p className='leading-trim font-semibold text-[16px] tracking-tight'>
-            {item.employeeEmail}
-          </p>
-          <div className='flex items-center mt-[10px]'>
-            <div className='mr-1'>
-              <Image src={phoneImage} alt='' />
-            </div>
-            <div className='text-[#5630FF] text-[12px] leading-[14px] tracking-[0] whitespace-nowrap text-capitalize inline-block'>
-              {item.employeePhone}
-            </div>
+          <div className='flex flex-row leading-trim font-medium text-[12px] tracking-tight'>
+            <p>Phone:</p>
+            <div className='text-[#9d9d9d]'>{item.employeePhone}</div>
           </div>
         </div>
-        <div className='w-[20%]'>
+        <div className='w-[25%]'>
+          <div className='flex flex-row leading-trim font-medium text-[12px] tracking-tight'>
+            <p>Email:</p>
+            <div className='text-[#9d9d9d]'>{item.employeeEmail}</div>
+          </div>
+        </div>
+        <div className='w-[10%]'>
           <span
-            className={`text-sm text-black h-6 ${
+            className={`text-sm text-black h-[19px] w-[50px] ${
               getStatusColor[item.employeeStatus as keyof EmployeestatusColor]
             } p-2 rounded-full`}>
             {item.employeeStatus}
           </span>
         </div>
-        {/* <div className='w-[25%]'>
-          <p className='text-[#5630FF] text-xs whitespace-nowrap text-capitalize inline-block'>
-            Assigned to
-          </p>
-          <p className='leading-[14px] text-black font-semibold text-[16px] tracking-[0] mt-[10px] whitespace-nowrap'>
-            {item.assignedToName}
-          </p>
-        </div> */}
+
         <div className='w-[2%]'>
           <Image
             className='cursor-pointer h-6 w-6'
             src={moreImage}
             alt=''
-            onClick={()=>{console.log('More button is clicked');}}
+            onClick={() => {
+              console.log('More button is clicked');
+            }}
           />
         </div>
       </div>
