a6de654557d0d7e13abcf8bfaad92adbc2886f62
Neaz Mahmood <neazn694@gmail.com>
2024-01-20 15:22:44 +0600

    trying to work with react-modal

---
+++ components/create-employee-modal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import * as Modal from 'react-modal';
+
+const CreateEmployeeModal = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+    console.log('modal-open');
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel='Example Modal'>
+        <h2>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+export default CreateEmployeeModal;
