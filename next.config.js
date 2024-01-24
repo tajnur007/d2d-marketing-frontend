74ee1141eaec52e5da26e6830ef0e4bd179d5f0c
Kazi Tajnur Islam <kazi.tajnur@vivacomsolutions.com>
2024-01-24 12:58:25 +0600

    Added https:// to avoid build error

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/proxy/:path*',
-        destination: `${process.env.API_BASE_URL}/${process.env.API_VERSION}/:path*`,
+        destination: `https://${process.env.API_BASE_URL}/${process.env.API_VERSION}/:path*`,
       },
     ];
   },
