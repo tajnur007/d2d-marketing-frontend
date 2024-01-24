709162807944cb289d4f08e0e267b52b41b5064b
Neaz Mahmood <neazn694@gmail.com>
2024-01-24 13:37:12 +0600

    fixed font, size, color of lead-details-drawer

--- components/lead-details.tsx
+++ components/lead-details.tsx
@@ -45,7 +45,7 @@ const LeadDetails = ({
         </div>
       </div>
 
-      <h4 className='text-[#00156A] font-medium text-[12px]'>Location</h4>
+      <h4 className='text-[#00156A] font-medium text-[12px] mb-[19px]'>Location</h4>
 
       <div className='flex items-center gap-4'>
         <div className='flex justify-between items-center gap-1'>
@@ -60,8 +60,8 @@ const LeadDetails = ({
       <div className='desc'>
         <div className='flex items-center gap-4 mt-3'>
           <div className='flex-grow break-all'>{data?.title}</div>
-          <div className='flex justify-between gap-2 px-2 py-1 rounded-xl items-center bg-[#FFD9D9] cursor-pointer'>
-            <button className=''>{data?.status}</button>
+          <div className='flex justify-between gap-2 px-2 py-[10px] rounded-xl items-center bg-[#FFD9D9] cursor-pointer'>
+            <button className='text-black text-sm font-medium'>{data?.status}</button>
             <Image src={downImage} alt='close' />
           </div>
         </div>
@@ -76,46 +76,74 @@ const LeadDetails = ({
         <AssignDropdownSelect />
       </div>
 
-      <div className='poc bg-[#EDEBF4] p-4 rounded-lg mt-4 whitespace-normal'>
-        <h4 className='text-[#5630FF] mb-2 text-[12px]'>Points of Contact</h4>
+      <div className='poc border-[#EDEBF4] bg-[#F8F8F8] p-4 rounded-lg mt-4 whitespace-normal'>
+        <h4 className='text-[#5630FF] font-medium mb-2 text-[12px] leading-[14px]'>
+          Points of Contact
+        </h4>
 
         <div className='rounded-lg bg-white mb-4 p-4'>
-          <div className='text-[#5630FF] mb-2 text-[12px]'>Name</div>
-          <div className='font-bold text-black text-[16px]'>{data?.assignedByName}</div>
+          <div className='text-[#5630FF] mb-2 text-[12px] font-medium leading-[14px]'>
+            Name
+          </div>
+          <div className='font-semibold leading-[14px] text-black text-[16px]'>
+            {data?.assignedByName}
+          </div>
         </div>
 
         <div className='rounded-lg bg-white mb-4 p-4'>
-          <div className='text-[#5630FF] mb-2 text-[12px]'>Phone</div>
-          <div className='font-bold text-black text-[16px]'>{data?.assignedByNumber}</div>
+          <div className='text-[#5630FF] mb-2 text-[12px] font-medium leading-[14px]'>
+            Phone
+          </div>
+          <div className='font-semibold leading-[14px] text-black text-[16px]'>
+            {data?.assignedByNumber}
+          </div>
         </div>
 
         <div className='rounded-lg bg-white mb-4 p-4'>
-          <div className='text-[#5630FF] mb-2 text-[12px]'>Email</div>
-          <div className='font-bold text-black text-[16px]'>{data?.assignedByEmail}</div>
+          <div className='text-[#5630FF] mb-2 text-[12px] font-medium leading-[14px]'>
+            Email
+          </div>
+          <div className='font-semibold leading-[14px] text-black text-[16px]'>
+            {data?.assignedByEmail}
+          </div>
         </div>
 
         <div className='rounded-lg bg-white mb-4 p-4'>
-          <div className='text-[#5630FF] mb-2 text-[12px]'>Reference</div>
-          <div className='font-bold text-black text-[16px]'>{data?.assignedToName}</div>
+          <div className='text-[#5630FF] mb-2 text-[12px] font-medium leading-[14px]'>
+            Reference
+          </div>
+          <div className='font-semibold leading-[14px] text-black text-[16px]'>
+            {data?.assignedToName}
+          </div>
         </div>
 
         <div className='rounded-lg bg-white p-4'>
-          <div className='text-[#5630FF] mb-2 text-[12px]'>Meeting notes</div>
-          <p className='font-bold text-black text-[16px]'>{data?.meetingNote}</p>
+          <div className='text-[#5630FF] mb-2 text-[12px] font-medium leading-[14px]'>
+            Meeting notes
+          </div>
+          <p className='font-semibold leading-[14px] text-black text-[16px]'>
+            {data?.meetingNote}
+          </p>
         </div>
       </div>
 
       <div className=''>
-        <h4 className='text-[#00156A] font-medium text-[12px] mt-5'>Image</h4>
+        <h4 className='text-[#00156A] font-medium text-[12px] leading-[14px] mt-5'>
+          Image
+        </h4>
 
         <Image src={leadImage} alt='image' className='w-[108px] h-[108px]' />
       </div>
 
       <div className='reminder bg-[#F8F6FF] p-4 rounded-lg mt-4 whitespace-normal'>
-        <div className='text-[#5630FF] mb-2 text-[12px]'>Reminder</div>
-        <div className='font-bold text-black'>{data?.reminder?.reminderTitle}</div>
-        <div className='text-[#8A8A8A]'>{data?.reminder?.reminderDate}</div>
-        <button className='bg-[#B8FFDD] py-1 px-2 rounded-full'>
+        <div className='text-[#5630FF] font-medium leading-[14px] mb-[10px] text-[12px]'>
+          Reminder
+        </div>
+        <div className='font-semibold text-base mb-[10px] text-black leading-[14px]'>
+          {data?.reminder?.reminderTitle}
+        </div>
+        <div className='text-[#8A8A8A] mb-[10px]'>{data?.reminder?.reminderDate}</div>
+        <button className='bg-[#B8FFDD] font-medium  text-black text-[10px] py-[5px] px-2 rounded-full'>
           {data?.reminder?.reminderStatus}
         </button>
       </div>
