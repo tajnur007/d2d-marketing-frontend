aa0dbe849f51a235d2afa9885e801c5551878361
Neaz Mahmood <neazn694@gmail.com>
2024-01-26 18:04:31 +0600

    forget password api complete

--- services/api-services.ts
+++ services/api-services.ts
@@ -25,4 +25,14 @@ export class ApiService {
 
     return resp;
   };
+
+  public resetPassword = async (data: any) => {
+      const resp: Response<any> = await this.client.request({
+      url: '/user/forget-password',
+      method: 'post',
+      data: data,
+    });
+
+    return resp;
+  };
 }
