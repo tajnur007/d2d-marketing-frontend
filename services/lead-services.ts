02731c0cdf3227b68f7782ad74cd08fb5bb1d7db
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-01-31 00:54:16 +0600

    Lead service upload image added

--- services/lead-services.ts
+++ services/lead-services.ts
@@ -71,4 +71,21 @@ export class LeadService {
     }
   };
 
+  public UploadLeadImage = async (data: any, token: string): Promise<any> => {
+    const config: AxiosRequestConfig = {};
+
+    if (token) {
+      config.headers = { Authorization: `Bearer ${token}` };
+    }
+
+    const resp = await this.client.request({
+      url: API_PATHS.UploadLeadImage,
+      method: API_METHODS.POST,
+      ...config,
+      data,
+    });
+
+    return resp;
+  };
+
 }
