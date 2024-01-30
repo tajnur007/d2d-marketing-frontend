646b90adcc028120c51e5ae5a7d4bb2de5e560f7
Hridoy <19202103103@cse.bubt.edu.bd>
2024-01-30 18:29:09 +0600

    Delete confirmation modal

--- utils/constants/common-constants.ts
+++ utils/constants/common-constants.ts
@@ -62,6 +62,7 @@ export const API_PATHS = {
   Leaderboard: '/user/leaderboard',
   GetLeads: '/lead/list',
   EmployeeListInfo:'/user/list',
+  DeleteLead: '/lead/delete',
 };
 
 export const SIDEBAR_ITEMS: SidebarItem[] = [
