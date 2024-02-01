40a01ee7fba0fbb49eb5d015380a3d5be05cd3b5
rkshaown <rkshawon36@gmail.com>
2024-02-01 20:29:21 +0600

    Updated loader component

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,7 +3,7 @@ import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
+    <div className='h-[80%] flex justify-center items-center'>
       <InfinitySpin width='200' color='#4f46e5' />
     </div>
   );
