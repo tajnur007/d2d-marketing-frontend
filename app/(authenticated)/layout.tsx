b8ef5ea5a58f4c112e98c2994c36b1a35e22b3c9
rkshaown <rkshawon36@gmail.com>
2024-02-08 13:15:19 +0600

    Updated layout page and merged with develop branch

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -7,6 +7,7 @@ import Sidebar from '../../components/layouts/sidebar';
 import { PAGE_ROUTES } from '@/utils/constants/common-constants';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
+import Loader from '@/components/loader';
 
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   const [loading, setloading] = useState(true);
@@ -21,21 +22,27 @@ export default function AuthenticatedLayout({ children }: { children: React.Reac
     if (!session?.data && session?.status === 'unauthenticated') {
       redirect(PAGE_ROUTES.Signin);
     }
-    if (path === '/employee-list' && userRole === 'executive') {
+    if (path.includes('/employee-list') && userRole === 'executive') {
       redirect(PAGE_ROUTES.Dashboard);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [path, session, userRole]);
 
-  if (session?.data && !loading) {
+  if (!session?.data && loading) {
     return (
-      <div className='flex h-screen bg-[#F7F7FB] overflow-hidden'>
-        <Sidebar userRole={userRole} />
-        <div className='flex-1 overflow-y-auto'>
-          <Navbar />
-          <Main>{children}</Main>
-        </div>
+      <div className='flex items-center justify-center h-screen bg-[#F7F7FB] '>
+        <Loader />
       </div>
     );
   }
+
+  return (
+    <div className='flex h-screen bg-[#F7F7FB] overflow-hidden'>
+      <Sidebar userRole={userRole} />
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+        <Main>{children}</Main>
+      </div>
+    </div>
+  );
 }
