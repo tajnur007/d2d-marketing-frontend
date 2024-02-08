518da0e259cfe5dc06bb02c5a1a6e627f6aeca51
Saidul-M-Khan <19-41766-3@student.aiub.edu>
2024-02-08 22:53:57 +0600

    [D2D-091] Forget Password Page Responsiveness Done

--- components/auth/reset-password.tsx
+++ components/auth/reset-password.tsx
@@ -12,7 +12,7 @@ import EmailSent from './email-sent';
 
 const ResetPassword = () => {
   const [email, setEmail] = useState('');
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [emailSubmitted, setEmailSubmitted] = useState(true); //! true For Testing
   const onFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     try {
       event.preventDefault();
@@ -42,9 +42,9 @@ const ResetPassword = () => {
 
   return (
     <section>
-      <div className='max-w-[600px] mx-auto px-6 py-12'>
+      <div className='md:max-w-[400px] lg:max-w-[600px] mx-auto lg:px-6 lg:py-9 md:py-2 md:px-3'>
         <div>
-          <div className='mt-10 my-6 text-[16px] flex items-center text-gray-500 tracking-[0] leading-[16px] whitespace-nowrap'>
+          <div className='md:mt-2 lg:mt-10 xl:mt-12 2xl:mt-14 md:my-3 lg:my-6 xl:my-9 2xl:my-12 md:text-[12px] lg:text-[16px] flex items-center text-gray-500 tracking-[0] leading-[16px] whitespace-nowrap'>
             <div className='mr-2'>
               <Link href={PAGE_ROUTES.Signin}>
                 <ArrowLeftCircleIcon />
@@ -52,29 +52,29 @@ const ResetPassword = () => {
             </div>
             <div>Back to Login</div>
           </div>
-          <div className='mt-10 text-[36px] font-bold leading-9 tracking-tight text-gray-900'>
+          <div className='md:mt-2 lg:mt-10 md:text-[18px] lg:text-[36px] font-bold leading-9 tracking-tight text-gray-900'>
             Reset password 👋
           </div>
-          <div className='my-5 text-[16px]'>
+          <div className='md:my-2 lg:my-5 md:text-[12px] lg:text-[16px]'>
             Enter your email and we&apos;ll send you instruction on
             <br />
             how to reset your password
           </div>
         </div>
 
-        <div className='mt-10'>
+        <div className='lg:mt-10 md:mt-3'>
           <form onSubmit={onFormSubmit}>
             <div>
-              <label className='font-semibold text-[#0B1420] text-[16px] tracking-[0.16px] leading-[16px] whitespace-nowrap'>
+              <label className='font-semibold text-[#0B1420] md:text-[12px] lg:text-[16px] tracking-[0.16px] leading-[16px] whitespace-nowrap'>
                 Email
               </label>
-              <div className='mt-4 mb-4'>
+              <div className='md:mt-2 lg:mt-4 md:mb-2 lg:mb-4'>
                 <input
                   id='email'
                   type='email'
                   name='email'
                   value={email}
-                  className='w-full rounded-[10px] border border-[#F3F3F3] outline-none border-solid py-4 px-3 placeholder-[#B9C1D9] text-[14px] font-medium focus-within:border-purple-500 focus-within:ring focus-within:ring-purple-200 transition-all duration-500'
+                  className='w-full rounded-[10px] border border-[#F3F3F3] outline-none border-solid lg:py-4 md:py-2 md:px-2 lg:px-3 placeholder-[#B9C1D9] md:text-[12px] lg:text-[14px] font-medium focus-within:border-purple-500 focus-within:ring focus-within:ring-purple-200 transition-all duration-500'
                   placeholder='Example@email.com'
                   autoComplete='off'
                   required
@@ -85,7 +85,7 @@ const ResetPassword = () => {
             <ForgetPasswordCommon
               buttonInfo={{
                 className:
-                  'w-full bg-[#4318FF] rounded-lg text-white p-4 font-normal my-4',
+                  'w-full bg-[#4318FF] rounded-lg text-white md:p-2 lg:p-4 font-normal md:my-2 lg:my-4 md:text-[12px] lg:text-[16px]',
                 text: 'Submit',
                 type: 'submit',
               }}
