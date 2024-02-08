7da0d65021784ee660e4e71e5fcb2426ced42690
rkshaown <rkshawon36@gmail.com>
2024-02-08 12:40:26 +0600

    Update sidebar component

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -7,43 +7,43 @@ import { useRouter, usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import profileImage from '@/assets/images/profile.png';
 import { Tooltip } from 'react-tooltip';
-import { signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 const Sidebar = ({ userRole }: { userRole: string | undefined }) => {
   const router = useRouter();
   const currentPage = usePathname();
-  const currPosition = SIDEBAR_ITEMS.find((item) => currentPage === item?.path);
+
+  let sidebarItems = [...SIDEBAR_ITEMS];
+  if (userRole === 'executive') {
+    sidebarItems = sidebarItems.filter((item) => item.iconName !== 'Employee List');
+    sidebarItems[2].position = 100;
+  }
+
+  const currPosition = sidebarItems.find((item) => currentPage === item?.path);
   const [selected, setSelected] = useState(currPosition?.position);
 
   const handleClick = (position: number, path: string) => {
-    if (userRole === 'executive' && path === '/settings') {
-      setSelected(position - 50);
-    } else {
-      setSelected(position);
-    }
+    setSelected(position);
     router.push(path);
   };
 
   useEffect(() => {
-    const position = currPosition?.position;
-    if (position && currentPage) {
-      handleClick(position, currentPage);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currPosition, currentPage]);
+    let position = currPosition?.position;
+    setSelected(position);
+  }, [currPosition, currentPage, userRole]);
 
   return (
     <div className='w-[88px] bg-white relative z-50'>
       <div
         className='absolute w-full flex justify-center top-5 cursor-pointer'
-        onClick={() => handleClick(SIDEBAR_ITEMS[0].position, SIDEBAR_ITEMS[0].path)}>
+        onClick={() => handleClick(sidebarItems[0].position, sidebarItems[0].path)}>
         <Logo />
       </div>
 
       <div className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col h-[147px] justify-between items-center w-full'>
-        {SIDEBAR_ITEMS.map(({ id, position, path, icon: Icon, iconName }) => (
+        {sidebarItems.map(({ id, position, path, icon: Icon, iconName }) => (
           <div
             key={id}
             onClick={() => handleClick(position, path)}
@@ -52,8 +52,6 @@ const Sidebar = ({ userRole }: { userRole: string | undefined }) => {
             data-tooltip-place='right'
             className={`flex items-center cursor-pointer transition-all duration-500 ${
               selected === position ? 'text-[#5630FF]' : 'text-[#69708C]'
-            } ${
-              userRole === 'executive' && path === '/employee-list' ? 'hidden' : 'block'
             }`}>
             {
               <div className='flex items-center px-8 py-3 hover:bg-purple-200 hover:duration-300 group'>
