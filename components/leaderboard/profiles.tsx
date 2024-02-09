3ac41d244c0f103c8f49838645a50b6c876edbd1
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-02-10 01:12:45 +0600

    responsiveness of dashboard page

--- components/leaderboard/profiles.tsx
+++ components/leaderboard/profiles.tsx
@@ -7,16 +7,16 @@ const Profiles = ({ data }: { data: Person[] | null }) => {
     <div className='h-[calc(100%-78px)] mb-6 px-5 overflow-y-auto tiny-scrollbar'>
       {data?.map((person: Person) => (
         <div key={person?.executive_id} className='flex items-center gap-5 mb-5'>
-          <div className='bg-[#E5DFFF] p-[13px] rounded-2xl font-semibold text-[#b8a9ff] text-[16px] w-[50px] flex items-center justify-center'>
+          <div className='bg-[#E5DFFF] p-[13px] rounded-2xl font-semibold text-[#b8a9ff] xl:text-[16px] text-[14px] xl:w-[50px] xl:h-full w-[40px] h-[35px] flex items-center justify-center'>
             {generateInitials(person?.executive_name)}
           </div>
           {/* width based on name length */}
           <div className='col-span-2'>
-            <div className=' font-semibold text-[#00156a] text-[16px]'>
+            <div className=' font-semibold text-[#00156a] xl:text-[16px] text-[14px]'>
               {person?.executive_name}
             </div>
-            <p className='text-[13px]  text-[#9aa1b1]'>
-              <span className='font-bold text-[#5630ff]  mr-1'>
+            <p className='xl:text-[13px] text-[11.5px] text-[#9aa1b1]'>
+              <span className='font-bold text-[#5630ff] mr-1'>
                 {person?.count_of_leads}
               </span>
               of leads
