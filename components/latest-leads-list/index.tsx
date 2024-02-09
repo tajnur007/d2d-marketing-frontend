22044c102767c7c76f6bbeab491e059801272aa1
Neaz Mahmood <neazn694@gmail.com>
2024-02-09 13:53:05 +0600

    fixed the null issue in leads-list

--- components/latest-leads-list/index.tsx
+++ components/latest-leads-list/index.tsx
@@ -21,6 +21,7 @@ const LatestLeadsList: React.FC = () => {
       try {
         const LeadServices = new LeadService();
         const response = await LeadServices.latestLeads(token);
+        console.log(response);
         setLatestLeads(response.data.Data);
         setIsLoading(false);
       } catch (error) {
@@ -33,34 +34,34 @@ const LatestLeadsList: React.FC = () => {
     }
   }, [token]);
 
- return (
-   <div className='w-full bg-white rounded-xl h-[calc(85vh-125px)] pb-6'>
-     <div className='sticky top-0 bg-white z-10 p-6 rounded-xl'>
-       <div className='flex justify-between items-center'>
-         <div className='flex items-center'>
-           <p className='font-bold text-[16px] tracking-[-0.32px] leading-[normal] whitespace-nowrap text-capitalize text-[#2B3674]'>
-             Latest Leads
-           </p>
-           <p className='flex items-center justify-center text-black  font-semibold text-capitalize text-[16px] h-6 bg-[#D2FBE7] rounded-full ms-2 p-2 leading-3 tracking-[-0.32px] whitespace-nowrap'>
-             {latestLeads?.Count}
-           </p>
-         </div>
-         <div>
-           <ViewAllLeadsButton />
-         </div>
-       </div>
-     </div>
-     {isLoading ? (
-       <Loader />
-     ) : (
-       <div className='w-full h-[calc(100%-88px)] overflow-y-auto tiny-scrollbar pl-6 whitespace-nowrap font-semibold text-[18px] leading-[normal]'>
-         {latestLeads?.Data?.map((item, index) => (
-           <LatestLeadRow key={index} item={item} />
-         ))}
-       </div>
-     )}
-   </div>
- );
+  return (
+    <div className='w-full bg-white rounded-xl h-[calc(85vh-125px)] pb-6'>
+      <div className='sticky top-0 bg-white z-10 p-6 rounded-xl'>
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center'>
+            <p className='font-bold text-[16px] tracking-[-0.32px] leading-[normal] whitespace-nowrap text-capitalize text-[#2B3674]'>
+              Latest Leads
+            </p>
+            <p className='flex items-center justify-center text-black  font-semibold text-capitalize text-[16px] h-6 bg-[#D2FBE7] rounded-full ms-2 p-2 leading-3 tracking-[-0.32px] whitespace-nowrap'>
+              {latestLeads?.Count}
+            </p>
+          </div>
+          <div>
+            <ViewAllLeadsButton />
+          </div>
+        </div>
+      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className='w-full h-[calc(100%-88px)] overflow-y-auto tiny-scrollbar pl-6 whitespace-nowrap font-semibold text-[18px] leading-[normal]'>
+          {latestLeads?.Data?.map((item, index) => (
+            <LatestLeadRow key={index} item={item} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default LatestLeadsList;
